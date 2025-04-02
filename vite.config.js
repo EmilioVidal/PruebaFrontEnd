76a6ecdff424bc0ab37096b1f6a9b1a4b2e11468
EmilioVidal <149820545+EmilioVidal@users.noreply.github.com>
2025-04-01 18:49:28 -0600

    cambio nombre base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/<nombre-de-tu-repo>/',
+    base: '/<PruebaFrontEnd>/',
     plugins: [react()],
 })

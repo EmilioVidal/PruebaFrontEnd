fa8fffe2fb024ad317d1019e59dbd1af98010815
EmilioVidal <149820545+EmilioVidal@users.noreply.github.com>
2025-04-01 18:50:29 -0600

    NOMBRE DEL REPO BASE

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/<PruebaFrontEnd>/',
+    base: '/PruebaFrontEnd/',
     plugins: [react()],
 })
